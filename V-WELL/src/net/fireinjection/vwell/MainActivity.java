49b3e6a8409ce6baa8debde99d38e60ce5d1f5a0
ruby <ruby@softwareinlife.com>
2013-10-06 12:14:15 +0900

    applied the android-annotations configurations.

--- V-WELL/src/net/fireinjection/vwell/MainActivity.java
+++ V-WELL/src/net/fireinjection/vwell/MainActivity.java
@@ -1,22 +1,13 @@
 package net.fireinjection.vwell;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
 
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.Fullscreen;
+
+@Fullscreen
+@EActivity(R.layout.activity_main)
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
 
 }
