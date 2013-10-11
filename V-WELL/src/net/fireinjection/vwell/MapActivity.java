d44349c14a7c090067a24413aa1c0d66cbb2c87a
ruby <ruby@softwareinlife.com>
2013-10-11 17:41:04 +0900

    gps

--- V-WELL/src/net/fireinjection/vwell/MapActivity.java
+++ V-WELL/src/net/fireinjection/vwell/MapActivity.java
@@ -1,5 +1,9 @@
 package net.fireinjection.vwell;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
@@ -12,8 +16,16 @@ import net.fireinjection.vwell.service.CommonViewService;
 import net.fireinjection.vwell.view.MenuAdapter;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 import android.widget.ListView;
@@ -39,8 +51,78 @@ public class MapActivity extends Activity implements MapView.OpenAPIKeyAuthentic
 	
 	private MapView mapView;
 
+	private final LocationListener locListener = new LocationListener() {
+
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			// TODO Auto-generated method stub
+			// GPS 상태 변화에 따른 행동을 적어주면 된다.
+			Log.e("#####", "onStatusChanged");
+		}
+
+		@Override
+		public void onProviderEnabled(String provider) {
+			// TODO Auto-generated method stub
+			// GPS 사용 가능으로 바꼈을 때 행동이다.
+			Log.e("#####", "onProviderEnabled");
+		}
+
+		@Override
+		public void onProviderDisabled(String provider) {
+			// TODO Auto-generated method stub
+			// GPS 사용 불가능으로 바꼈을 때 행동을 적어주면 된다.
+			Log.e("#####", "onProviderDisabled");
+		}
+
+		@Override
+		public void onLocationChanged(Location location) {
+			// TODO Auto-generated method stub
+			// 바뀐 위치를 수신하였을 경우 수행되는 부분이다.
+			Log.e("#####", "onLocationChanged");
+		}
+	};
+	
 	@AfterInject
 	void afterInject(){
+		LocationManager locManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+		String provider = LocationManager.GPS_PROVIDER; // GPS_PROVIER 로 설정
+		Location loc = locManager.getLastKnownLocation(provider); // 마지막으로 기억하는 위치를 설정한다.
+		// 주소를 확인하기 위한 Geocoder KOREA 와 KOREAN 둘다 가능
+		Geocoder geoCoder = new Geocoder(this, Locale.KOREAN); 
+		// GPS 를 1초(1000)마다 또는 10미터 이동시마다 위치값을 받아온다.
+		locManager.requestLocationUpdates(provider, 10000, 10, locListener);
+		
+		if (loc != null) { // 현제 지역의 값이 있을경우만 아래 코드 실행
+			double lat = loc.getLatitude(); // 두 줄의 코드는 현제 위치(loc)의 위/경도를 받아온다.
+			double lng = loc.getLongitude();
+
+			Log.e("#####", "loc.getLatitude() : " + loc.getLatitude());
+			Log.e("#####", "loc.getLongitude() : " + loc.getLongitude());
+
+			// 위경도를 이용하여 현제 위치의 주소를 받아온다. 세번째 인자는 현제 위치의 주소형태가 여러가지인데 그 중 몇가지를
+			// 받아올 것인가를 설정한다. (인천 / 인천시 연수구 / 인천시 연수구 송도동 등으로 여러가지가 나온다. 다음줄에
+			// 중단점을 잡고 직접 확인해보면 좋을것이다.
+			List<Address> addresses;
+			try {
+				addresses = geoCoder.getFromLocation(lat, lng, 5);
+				if (addresses.size() > 0) { // 받아온 주소가 있을 경우
+					// 첫번째 주소를 Address 형식으로 받아온다. 필자는 첫번째(가장 자세한 주소값이 들어가있다) 만 사용한다.
+					Address mAddress = addresses.get(0);
+					// 아래와 같은 방법으로 빼내면 되며 시/구/동 이 저장된 변수에 대해서는 중단점을 잡고 Address 객체의
+					// 값들을 확인해 보면서 하면 되겠다.
+					String Area = mAddress.getAdminArea();
+					// mAddress 를 가공하여 원하는 주소를 빼내면 되며 밑에 코드는 필자가 String 형식으로 뽑아내는
+					// 방법이다.
+					// 원래는 Address 안에 국가/도시/구/동 별로 따로 저장되어 있지만 가끔 구 가 빠져있던가 하는 예외가
+					// 생기므로
+					// (완전 빠졌다는게 아니라 구가 들어가야 하는 변수에 안들어가있다던가 하는 경우)
+					// 내가 원하는 방법으로 직접 String을 자른 부분이다.
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	@AfterViews
